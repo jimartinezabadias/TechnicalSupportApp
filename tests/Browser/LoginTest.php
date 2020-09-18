5f33333da72ecd81e9b2218b2c7e5a097a95f140
jimartinezabadias <59533825+jimartinezabadias@users.noreply.github.com>
2020-09-18 13:47:13 -0300

    Application Login Tests: Succeed and failed loging (Closes #6) (#7)

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+
+    use DatabaseMigrations;
+
+
+    public function testLogin()
+    {
+        
+        $user = User::factory()->create([
+            'email' => 'test@laravel.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password','12345678')
+                    ->press('LOGIN')
+                    ->assertSee('Dashboard');
+        });
+
+    }
+
+    public function testLoginFail()
+    {
+
+        $user = User::factory()->create([
+            'email' => 'test@laravel.com',
+            'password' => bcrypt('123456789')
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password','12345678')
+                    ->press('LOGIN')
+                    ->assertSee('These credentials do not match our records.');
+        });
+
+    }
+}
