557510afcc8d428ab1bb927a5c6fbcd765031834
Juan Ignacio <jimartinezabadias@gmail.com>
2020-09-29 18:59:26 -0300

    Service: Create model and migration (Closes #12)

---
+++ database/migrations/2020_09_29_214311_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            // machine ref
+            $table->string('failure');
+            $table->date('date');
+            $table->decimal('price',10,2);
+            $table->text('failure_description');
+            $table->text('service_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
