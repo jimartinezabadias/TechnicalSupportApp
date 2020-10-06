a5358cd863ad1aea3add7e1d11fe225bf9e38ecc
Juan Ignacio <jimartinezabadias@gmail.com>
2020-10-06 14:01:11 -0300

    Add Test for private Urls (Closes #42)

---
+++ tests/Feature/PrivateUrlsTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PrivateUrlsTest extends TestCase
+{
+   
+    private $loginUrl = "/login";
+    
+    /**
+     * Non logged in must redirect.
+     *
+     * @return void
+     */
+    public function testMachineNotLoggedIn()
+    {
+        $response = $this->get('/machines');
+
+        $response->assertRedirect($this->loginUrl);
+    }
+
+    /**
+     * Non logged in must redirect.
+     *
+     * @return void
+     */
+    public function testServiceNotLoggedIn()
+    {
+        $response = $this->get('/services');
+
+        $response->assertRedirect($this->loginUrl);
+    }
+
+    /**
+     *  Logged in must show Machines view.
+     *
+     * @return void
+     */
+    public function testMachineLoggedIn()
+    {
+        $user = User::factory()->create();
+        
+        $response = $this->actingAs($user)
+                         ->get('/machines');
+
+        $response->assertSee('All Machines');
+    }
+
+    /**
+     *  Logged in must show Machines view.
+     *
+     * @return void
+     */
+    public function testServiceLoggedIn()
+    {
+        $user = User::factory()->create();
+        
+        $response = $this->actingAs($user)
+                         ->get('/services');
+
+        $response->assertSee('All Services');
+    }
+    
+}
