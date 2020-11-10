6d02398186d340712831258fe6b7e3d72b7ffafc
Juan Ignacio <jimartinezabadias@gmail.com>
2020-11-09 23:55:12 -0300

    Add select user when creating a machine (Closes #88)

--- resources/views/machines/create.blade.php
+++ resources/views/machines/create.blade.php
@@ -33,6 +33,33 @@
                                     @enderror
 
                                 </div>
+                                
+                                <div class="col-span-6 sm:col-span-4">
+                                    
+                                    <label for="user" class='block font-medium text-sm text-gray-700'>Client user</label>
+                                    <div class="flex-shrink w-full inline-block relative">
+                                        <select id="user" name="user_id" value="{{ old('user_id') }}"
+                                            class="block appearance-none text-gray-600 w-full bg-white border border-gray-400 shadow-inner px-4 py-2 pr-8 rounded">
+                                            <option>Sin Usuario</option>
+                                            @foreach($clients as $client)
+
+                                                <option value="{{ $client->id }}" 
+                                                    @if( old('client_id') == $client->id ) selected @endif
+                                                    >{{ $client->name }} Email: {{ $client->email }}</option>
+
+                                            @endforeach
+                                            
+                                        </select>
+                                        <div class="pointer-events-none absolute top-0 mt-3  right-0 flex items-center px-2 text-gray-600">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                                        </div>
+                                    </div>
+
+                                    @error('user_id')
+                                        <div class="text-red-600">{{ $message }}</div>
+                                    @enderror
+
+                                </div>
 
                                 <div class="col-span-6 sm:col-span-4">
                     
@@ -41,7 +68,7 @@
                                     </label> -->
 
                                     <!-- <div class='w-full md:w-full px-3 mb-6'> -->
-                                        <label for="type" class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Type</label>
+                                        <label for="type" class='block font-medium text-sm text-gray-700'>Type</label>
                                         <div class="flex-shrink w-full inline-block relative">
                                             <select id="type" name="type" value="{{ old('type') }}"
                                                 class="block appearance-none text-gray-600 w-full bg-white border border-gray-400 shadow-inner px-4 py-2 pr-8 rounded">
