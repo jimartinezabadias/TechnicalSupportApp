d9bd0125e9c9097cdd78e4a797597b427808aebe
Carlos Alcca <edual986@gmail.com>
2020-11-18 15:29:29 -0300

    implement test API get machines

---
+++ tests/Feature/MachineApiTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Machine;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class MachineApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetMachinesApi()
+    {
+        $user = User::factory()->create();
+
+        $machine = Machine::factory()->create(
+            [
+                "assigned_to" => $user->id
+            ]
+        );
+
+        Sanctum::actingAs(
+            $User,
+            ['view machines']
+        );
+
+        $response = $this->getJson('/api/machines');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1);
+        $response->assertJson([
+            "assigned_to" => $user->id
+        ]);
+    }
+}
